fd12fb7a536fb5e040cf8359153175909e795c01
ibrahimballamsf <ibrahim.balla@brussels.msf.org>
2023-02-21 23:51:31 +0100

    Build Docker File

--- hello.py
+++ hello.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Hello, My Name is Ibrahim Yerima Balla This is my Cloud DevOps Capstone'
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=80)
