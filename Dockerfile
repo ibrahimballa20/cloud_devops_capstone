4f21de9d359b507e04cc91680ca8ee5636ceaf50
ibrahimballamsf <ibrahim.balla@brussels.msf.org>
2023-02-21 23:42:28 +0100

    Build Docker File

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM python:3.9-buster
 
 WORKDIR /app
 
-COPY . hello.py /app/
-
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-    
+
+COPY . .
+
 EXPOSE 80
 
 CMD ["python", "hello.py"]
--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>Hello  My Name is Ibrahim Yerima Balla, Welcome to my Capstone Project:)</h1>"
+def hello_world():
+    return 'Hello, My Name is Ibrahim Yerima Balla This is my Cloud DevOps Capstone'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
